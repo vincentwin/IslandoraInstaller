f4c93ef0ca454c8816a0ea1bb97f27ad674bff3e
Vincent Nguyen <vincentcgi@gmail.com>
2012-10-10 09:28:14 -0400

    Moved modules

--- Drupal/sites/default/modules/islandora_xml_forms/elements/js/datepicker.js
+++
@@ -1,7 +0,0 @@
-Drupal.behaviors.xmlFormElementDatepicker = function(context) {
-    $( "input.datepicker" ).datepicker({
-	    changeMonth: true,
-	    changeYear: true,
-	    dateFormat: 'yy-mm-dd'
-	});
-}
